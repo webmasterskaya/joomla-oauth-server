f5fe779e5d6c2c1b32680711a4352085775d07d6
Artem Vasilev <kern.usr@gmial.com>
2024-03-04 03:21:36 +0300

    Обновил DDL для #__webmasterskaya_oauthserver_clients

--- com_oauthserver/administrator/sql/install.mysql.utf8.sql
+++ com_oauthserver/administrator/sql/install.mysql.utf8.sql
@@ -1,12 +1,15 @@
 CREATE TABLE IF NOT EXISTS `#__webmasterskaya_oauthserver_clients`
 (
-    `id`           int unsigned NOT NULL AUTO_INCREMENT,
-    `client_name`  varchar(150) NOT NULL,
-    `client_token` varchar(255) NOT NULL,
-    `client_id`    varchar(255) NOT NULL,
+    id                    int unsigned auto_increment,
+    name                  varchar(150)      not null,
+    identifier            varchar(255)      not null,
+    secret                varchar(255)      null,
+    public                tinyint default 0 not null,
+    redirect_uri          varchar(255)      null,
+    allow_plain_text_pkce tinyint default 1 not null,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `client_token` (`client_token`),
-    UNIQUE KEY `client_id` (`client_id`)
+    UNIQUE KEY `secret` (`identifier`, `secret`),
+    UNIQUE KEY `identifier` (`identifier`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   DEFAULT COLLATE = utf8mb4_unicode_ci;
