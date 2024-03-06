dc4b1734ab7e7cd60b0e0fb6aefd94043e8142e6
Artem Vasilev <kern.usr@gmial.com>
2024-03-06 12:51:01 +0300

    Вернул отдельную реализацию Client Entity

---
+++ com_oauthserver/site/src/Entity/Client.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Webmasterskaya\Component\OauthServer\Site\Entity;
+
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\Traits\ClientTrait;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+
+class Client implements ClientEntityInterface
+{
+    use ClientTrait;
+    use EntityTrait;
+
+    /**
+     * @var bool
+     */
+    private $allowPlainTextPkce = true;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string[] $redirectUri
+     */
+    public function setRedirectUri(array $redirectUri): void
+    {
+        $this->redirectUri = $redirectUri;
+    }
+
+    public function setConfidential(bool $isConfidential): void
+    {
+        $this->isConfidential = $isConfidential;
+    }
+
+    public function isPlainTextPkceAllowed(): bool
+    {
+        return $this->allowPlainTextPkce;
+    }
+
+    public function setAllowPlainTextPkce(bool $allowPlainTextPkce): void
+    {
+        $this->allowPlainTextPkce = $allowPlainTextPkce;
+    }
+}
