fa0a116d5c8dbca65f45eef6e945df712c48e227
Artem Vasilev <kern.usr@gmial.com>
2024-03-08 02:26:27 +0300

    Добавил sql удаления

---
+++ com_oauthserver/administrator/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,4 @@
+drop table if exists `#__webmasterskaya_oauthserver_access_tokens`;
+drop table if exists `#__webmasterskaya_oauthserver_authorization_codes`;
+drop table if exists `#__webmasterskaya_oauthserver_clients`;
+drop table if exists `#__webmasterskaya_oauthserver_refresh_tokens`;
