0f754f8c7b161f80c229cfcbbaefbfc30f493150
Artem Vasilev <kern.usr@gmial.com>
2024-03-08 02:25:08 +0300

    Добавил sql установки

--- com_oauthserver/administrator/sql/install.mysql.utf8.sql
+++ com_oauthserver/administrator/sql/install.mysql.utf8.sql
@@ -1,15 +1,57 @@
-CREATE TABLE IF NOT EXISTS `#__webmasterskaya_oauthserver_clients`
+create table if not exists `#__webmasterskaya_oauthserver_access_tokens`
 (
-    id                    int unsigned auto_increment,
-    name                  varchar(150)      not null,
-    identifier            varchar(255)      not null,
-    secret                varchar(255)      null,
+    id         int auto_increment
+        primary key,
+    identifier varchar(80)          not null,
+    expiry     datetime             not null,
+    user_id    int                  null,
+    scopes     text                 null,
+    client_id  int                  not null,
+    revoked    tinyint(1) default 0 not null,
+    constraint oauthserver_access_tokens_uk_1
+        unique (identifier)
+);
+
+create table `#__webmasterskaya_oauthserver_authorization_codes`
+(
+    id         int auto_increment
+        primary key,
+    identifier varchar(80)          not null,
+    expiry     datetime             not null,
+    user_id    int                  null,
+    scopes     text                 null,
+    revoked    tinyint(1) default 0 not null,
+    client_id  int                  not null,
+    constraint oauthserver_authorization_codes_uk_1
+        unique (identifier)
+);
+
+create table `#__webmasterskaya_oauthserver_clients`
+(
+    id                    int auto_increment
+        primary key,
+    identifier            varchar(32)       not null,
+    name                  varchar(128)      not null,
+    secret                varchar(128)      null,
+    redirect_uris         longtext          null,
+    grants                longtext          null,
+    scopes                longtext          null,
+    active                tinyint default 1 not null,
     public                tinyint default 0 not null,
-    redirect_uri          varchar(255)      null,
     allow_plain_text_pkce tinyint default 1 not null,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `secret` (`identifier`, `secret`),
-    UNIQUE KEY `identifier` (`identifier`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  DEFAULT COLLATE = utf8mb4_unicode_ci;
+    constraint oauthserver_clients_uk_1
+        unique (identifier)
+);
+
+create table `#__webmasterskaya_oauthserver_refresh_tokens`
+(
+    id              int auto_increment
+        primary key,
+    identifier      varchar(80)          not null,
+    expiry          datetime             not null,
+    revoked         tinyint(1) default 0 not null,
+    access_token_id int                  null,
+    constraint oauthserver_refresh_tokens_uk_2
+        unique (identifier)
+);
+
