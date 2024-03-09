373de2234c36ee746d548f418562e8f3e934a4b2
Artem Vasilev <kern.usr@gmail.com>
2024-03-09 16:50:16 +0300

    _JEXEC & copyright

--- com_oauthserver/administrator/src/Dispatcher/Dispatcher.php
+++ com_oauthserver/administrator/src/Dispatcher/Dispatcher.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ * @package         Joomla.Administrator
+ * @subpackage      com_oauthserver
+ *
+ * @copyright   (c) 2024. Webmasterskaya. <https://webmasterskaya.xyz>
+ * @license         MIT; see LICENSE.txt
+ **/
 
 namespace Webmasterskaya\Component\OauthServer\Administrator\Dispatcher;
 
 use Joomla\CMS\Dispatcher\ComponentDispatcher;
-use Joomla\CMS\MVC\Controller\BaseController;
+
+\defined('_JEXEC') or die;
 
 class Dispatcher extends ComponentDispatcher
 {
