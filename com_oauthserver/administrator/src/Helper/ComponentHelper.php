9a4ff028f4ef7d3eb6b219d6525ed262d69fd106
Artem Vasilev <kern.usr@gmail.com>
2024-03-09 17:03:55 +0300

    _JEXEC & copyright

--- com_oauthserver/administrator/src/Helper/ComponentHelper.php
+++ com_oauthserver/administrator/src/Helper/ComponentHelper.php
@@ -1,22 +1,36 @@
 <?php
+/**
+ * @package         Joomla.Administrator
+ * @subpackage      com_oauthserver
+ *
+ * @copyright   (c) 2024. Webmasterskaya. <https://webmasterskaya.xyz>
+ * @license         MIT; see LICENSE.txt
+ **/
 
 namespace Webmasterskaya\Component\OauthServer\Administrator\Helper;
 
+\defined('_JEXEC') or die;
+
 abstract class ComponentHelper
 {
+    /**
+     * Creates a Composer autoloader, unregisters it, and registers it again at the end of the autoloader stack.
+     *
+     * @return void
+     * @since version
+     */
     public static function registerComponentDependencies(): void
     {
         static $registered;
 
-        if (!isset($registered)) {
+        if (!isset($registered))
+        {
             /** @var \Composer\Autoload\ClassLoader $loader */
             $loader = require JPATH_ADMINISTRATOR . '/components/com_oauthserver/vendor/autoload.php';
 
             $loader->unregister();
 
-            if (spl_autoload_register([new \Joomla\CMS\Autoload\ClassLoader($loader), 'loadClass'])) {
-                $registered = true;
-            }
+            $loader->register();
         }
     }
 }
